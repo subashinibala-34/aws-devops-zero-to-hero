4ca30ee44c8396bfc12eb77bd47837281244261a
subashinibala-34 <subabala1299@gmail.com>
2025-05-20 03:30:15 +0530

    file removed12

--- day-14/scripts/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull subashinibala/simple-python-flask-app:latest
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 subashinibala/simple-python-flask-app
