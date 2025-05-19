b617a7801e0a4e87440b566c2a3829d0b7d803db
subashinibala-34 <subabala1299@gmail.com>
2025-05-20 03:03:06 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,10 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+container_id=$(docker ps -q)
+if [ -n "$container_id" ]; then
+  docker rm -f $container_id
+  echo "Stopped and removed container(s): $container_id"
+else
+  echo "No running containers to stop."
+fi
